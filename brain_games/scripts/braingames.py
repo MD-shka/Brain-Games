e655e02ff2c063fdd2c826669338bccdbb6a5c6b
Mikhail Egorov <pe.egorov@gmail.com>
2023-12-01 06:39:37 +0600

    brain_games/scripts/braingames.py

--- brain_games/scripts/braingames.py
+++ brain_games/scripts/braingames.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
 from brain_games import cli
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    print(cli.welcome_user())
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
