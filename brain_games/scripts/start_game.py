9a5aba6d1520d965a5f88ac1f1f4b32c2bcfed92
Mikhail Egorov <pe.egorov@gmail.com>
2023-12-03 03:15:07 +0600

    update brain_games/scripts/start_game.py

--- brain_games/scripts/start_game.py
+++ brain_games/scripts/start_game.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 import sys
 from brain_games import engine
-from brain_games.games import even
+from brain_games.games import even, calc
 
 
 def brain_even():
     engine.game_engine(even)
 
 
+def brain_calc():
+    engine.game_engine(calc)
+
+
 if __name__ == '__main__':
     if sys.argv[0] == 'brain-even':
         brain_even()
+
+    elif sys.argv[0] == 'brain-calc':
+        brain_calc()
