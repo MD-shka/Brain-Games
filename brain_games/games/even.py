822fa0d6f4814540c78660cca9cc9cde008d8b12
Mikhail Egorov <pe.egorov@gmail.com>
2023-12-03 13:45:08 +0600

    fix brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,13 +1,17 @@
 import random
 
 
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
 def show_rules():
     return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def solution():
     question = random.randint(1, 999)
-    answer = 'no'
-    if question % 2 == 0:
-        answer = 'yes'
+    answer = ('no', 'yes')[is_even(question)]
     return question, answer
